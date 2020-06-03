d1c9082538192dc177f144dfe6e9e83f4501cf9f
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-03 14:46:20 -0400

    Add files via upload
    Add files via upload

---
+++ Car-Insurance-Approval-Program/Car-Insurance-Approval-Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Approval_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string answer = Console.ReadLine();
+            int age = Convert.ToInt32(answer);
+
+            Console.WriteLine("True or False: You have had a DUI");
+            string duiAnswer = Console.ReadLine();
+            bool dui = Convert.ToBoolean(duiAnswer);
+            
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speed = Console.ReadLine();
+            int speedingTickets = Convert.ToInt32(speed);
+
+            Console.WriteLine("Qualified?");
+
+
+            bool isQualified = (age > 15 && dui == false && speedingTickets <= 3);
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+        }
+    }
+}
