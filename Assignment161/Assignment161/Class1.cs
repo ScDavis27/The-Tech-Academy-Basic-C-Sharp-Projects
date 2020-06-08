8320099e0ea1723351609a7641e6eb3f8bfec1cd
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-08 19:51:17 -0400

    Add files via upload
    Add files via upload

---
+++ Assignment161/Assignment161/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment161
+{
+    class Class1
+    {
+        public static int Sum(int x, int y)
+        {
+            int a = x;
+            int b = y;
+
+            int result = a + b;
+
+            return result;
+        }
+
+
+        public static int Product(int x, int y)
+        {
+            int a = x;
+            int b = y;
+
+            int result = a * b;
+
+            return result;
+        }
+
+
+        public static int Divide(int x, int y)
+        {
+            int a = x;
+            int b = y;
+
+            int result = a / b;
+
+            return result;
+        }
+        
+    }
+}
---
+++ Assignment161/Assignment161/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment161
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose a number to do 3 seperate math operations on:");
+            string userInt = Console.ReadLine();
+            int x = Convert.ToInt32(userInt);
+
+            int result = Class1.Sum(x,7);
+            Console.WriteLine("The sum of " + x + " and 7 is " + result);
+            Console.ReadLine();
+
+            int result1 = Class1.Product(x, 7);
+            Console.WriteLine(x + " mutiplied by 7 equals " + result1);
+            Console.ReadLine();
+
+            int result2 = Class1.Divide(x, 7);
+            Console.WriteLine(x + " divided by 7 equals " + result2);
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
