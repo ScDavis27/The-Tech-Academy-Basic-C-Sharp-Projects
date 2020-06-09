9e738995c588650e7e856427bdfad9d8e6e44aa6
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-09 15:15:15 -0400

    Add files via upload
    Add files via upload

---
+++ Assignment165/Assignment165/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment165
+{
+    class Class1
+    {
+        public static int MyMethod(int x, int y = 2)
+        {
+            int result = x * y;
+
+            return result;
+            
+        }
+    }
+}
---
+++ Assignment165/Assignment165/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            string userInput = Console.ReadLine();
+            int x = Convert.ToInt32(userInput);
+            Console.WriteLine("(Optional)Please enter another number: ");
+            string userInput2 = Console.ReadLine();
+            
+            if (userInput2 != "")
+            {
+                int y = Convert.ToInt32(userInput2);
+                Console.WriteLine(Class1.MyMethod(x, y));
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(Class1.MyMethod(x));
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
