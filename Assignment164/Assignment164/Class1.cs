17a8ba5ab0379467ffa814aacc0326a4d4b8cc8e
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-09 12:36:21 -0400

    Add files via upload
    Add files via upload

---
+++ Assignment164/Assignment164/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment164
+{
+    class Class1
+    {
+        public static int Multiply(int x, int y)
+        {
+            
+            int result = x * y;
+
+            return result;
+        }
+
+        public static decimal Multiply(decimal x, int y)
+        {
+            double result = (int)(x * y);
+
+            return (decimal)result;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            double result = x * y;
+
+            return result;
+        }
+    }
+}
---
+++ Assignment164/Assignment164/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to multiply by 7:");
+            string myInt = Console.ReadLine();
+            int y = Convert.ToInt32(myInt);
+            Console.WriteLine(Class1.Multiply(7, y));
+            Console.ReadLine();
+
+
+            Console.WriteLine("Please enter a decimal number: ");
+            string userInput = Console.ReadLine();
+            decimal userDec = Convert.ToDecimal(userInput);
+            Console.WriteLine(Class1.Multiply(userDec, 7));
+            Console.ReadKey();
+
+            Console.WriteLine("Please enter a number to multiply by 17: ");
+            string userString = Console.ReadLine();
+            double userDouble = Convert.ToDouble(userString);
+            Console.WriteLine(Class1.Multiply(userDouble, 17));
+            Console.ReadLine();
+        }
+    }
+}
