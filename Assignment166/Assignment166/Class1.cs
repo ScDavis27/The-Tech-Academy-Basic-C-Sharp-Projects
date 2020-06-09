de302667f80f4bd83e332e346624e0337a948154
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-09 19:10:42 -0400

    Add files via upload
    Add files via upload

---
+++ Assignment166/Assignment166/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment166
+{
+    class Class1
+    {
+        public static void MyMethod(int intOne, int intTwo)
+        {
+            int result = intOne * 7;
+            Console.WriteLine(intOne + " multiplied by 7 = " + result);
+            Console.WriteLine(intTwo);
+            return;
+        }
+
+    }
+}
