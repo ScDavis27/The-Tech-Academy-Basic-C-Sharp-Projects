cb85260d97795d36241a71c71ea605802cdd8ba2
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-10 15:40:09 -0400

    Add files via upload
    Add files via upload

---
+++ SuperclassMethod/SuperclassMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperclassMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1 };
+            
+
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
