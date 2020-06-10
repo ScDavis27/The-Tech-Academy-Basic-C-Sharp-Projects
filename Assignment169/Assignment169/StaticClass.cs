c83210c4fc6c6c2c7aacdea9c9ad97dcf8179130
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-10 12:33:09 -0400

    Add files via upload
    Add files via upload

---
+++ Assignment169/Assignment169/StaticClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment169
+{
+    static class StaticClass 
+    {
+        static string f_Name = "Scott";
+        static string l_Name = "Davis";
+
+        public static void details()
+        {
+            Console.WriteLine("First name: " + f_Name);
+            Console.WriteLine("Last name: " + l_Name);
+            Console.ReadLine();
+        }
+
+
+    }
+}
