1fb523ba97f763756aea1e661c8cf4ef3acfbea6
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-12 14:41:05 -0400

    Add files via upload
    Add files via upload

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number decNumber = new Number();
+            decNumber.Amount = 17.7m;
+            Console.WriteLine(decNumber.Amount);
+            Console.ReadLine();
+        }
+    }
+}
