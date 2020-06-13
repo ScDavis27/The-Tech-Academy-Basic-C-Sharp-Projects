3b14bd650b4673db5f86d75a3699645ee565abde
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-13 19:12:51 -0400

    Add files via upload
    Add files via upload

---
+++ ConstVar/ConstVar/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Employee()
+        {
+        }
+        
+        public Employee(string name) : this(name, 5)
+        {
+            this.Name = name;
+        }
+        public Employee(int id)
+        {
+            this.Id = id;
+        }
+        public Employee(string name, int id)
+        {
+        }
+
+
+    }
+}
---
+++ ConstVar/ConstVar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var newPlayer = Console.ReadLine();
+            const string myName = "Scott";
+            Console.WriteLine("Hello, {0}, my name is {1}, welcome to my program!", newPlayer, myName);
+            Console.ReadLine();
+
+            Employee employee = new Employee("Johnny Jones", 7);
+            
+            Console.WriteLine(employee.Name);
+            Console.WriteLine(employee.Id);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
