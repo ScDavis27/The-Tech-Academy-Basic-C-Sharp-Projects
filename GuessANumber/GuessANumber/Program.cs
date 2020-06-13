6fea9d37df8144f3dc13836ab8f6bab2f91a05b4
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-13 12:37:03 -0400

    Add files via upload
    Add files via upload

---
+++ GuessANumber/GuessANumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Guess a number:");
+            string userNum = (Console.ReadLine());
+            File.WriteAllText(@"C:\Users\Student\Logs\Logs.txt", userNum);
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Student\Logs\Logs.txt"));
+            Console.ReadLine();
+        }
+    }
+}
