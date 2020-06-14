511b5f769ab428f97680bb291b00d31ad67daca1
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-14 13:11:07 -0400

    Add files via upload
    Add files via upload

---
+++ ExceptionHandling2/ExceptionHandling2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                DateTime current = DateTime.Now;
+                Console.WriteLine("What is your current age?");
+                string userAge = Console.ReadLine();
+                int age = Convert.ToInt32(userAge);
+                DateTime yearBorn = current.AddYears(-age);
+                int year = yearBorn.Year;
+                Console.WriteLine(year);
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter just the year in digits only.");
+                Console.ReadLine();
+            }
+
+            
+            
+        }
+    }
+}
+
