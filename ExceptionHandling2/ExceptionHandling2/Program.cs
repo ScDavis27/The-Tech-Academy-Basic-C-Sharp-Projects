a79f1999628f0d5edfe93b20b663a842ffe7b158
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-06-14 15:33:43 -0400

    Add files via upload
    Add files via upload

--- ExceptionHandling2/ExceptionHandling2/Program.cs
+++ ExceptionHandling2/ExceptionHandling2/Program.cs
@@ -10,34 +10,24 @@ namespace ExceptionHandling2
     {
         static void Main(string[] args)
         {
-            
+
             try
             {
                 Console.WriteLine("What is your current age?");
                 string userAge = Console.ReadLine();
                 int age = Convert.ToInt32(userAge);
-                if (age > 0)
-                {
-                    DateTime current = DateTime.Now;
-                    DateTime yearBorn = current.AddYears(-age);
-                    int year = yearBorn.Year;
-                    Console.WriteLine(year);
-                    Console.ReadLine();
-                }
-                else
-                {
-                    Console.WriteLine("Please enter just the year in digits only.");
-                    Console.ReadLine();
-                }
+                int months = age * 12;
+                DateTime current = DateTime.Now;
+                DateTime yearBorn = current.AddMonths(-months);
+                int year = yearBorn.Year;
+                Console.WriteLine("You were born in the year " + year);
+                Console.ReadLine();
             }
             catch (Exception)
             {
                 Console.WriteLine("Something went wrong. Please try again.");
                 Console.ReadLine();
             }
-
-
-
         }
     }
 }
